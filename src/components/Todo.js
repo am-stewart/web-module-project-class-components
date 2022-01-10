98514ba11e6f97c975ab710f22107b8c39220e80
Allison Stewart <amstewart@yahoo.com>
2022-01-10 16:40:07 -0500

    set up components

---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Todo extends React.Component {
+
+render() {
+    return (
+        <div>
+            <h3>To Do</h3>
+        </div>
+    )
+  }
+}
+
+export default Todo;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Todo from './Todo'
+
+class TodoForm extends React.Component {
+
+render() {
+    return (
+        <div>
+            <h3>To Do Form</h3>
+        </div>
+    )
+  }
+}
+
+export default TodoForm;
